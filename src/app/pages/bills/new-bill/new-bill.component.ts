bd58c9cfa64632c5ed7c61a25b0a16c4b9a49bb4
Or Bruchim <orbruhim@gmail.com>
2022-09-17 20:42:29 +0300

    bills.component.ts refactor

--- src/app/pages/bills/new-bill/new-bill.component.ts
+++ src/app/pages/bills/new-bill/new-bill.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 import { BillsFacade } from '../sotre/bills.facade';
-import { DataStorageService } from '../../../data-storage.service';
 import { BillsService } from '../sotre/bills.service';
-import { Bill } from '../bill.interface';
+import { takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-bill',
@@ -15,18 +14,39 @@ import { Bill } from '../bill.interface';
 export class NewBillComponent implements OnInit, OnDestroy {
   title: string = '';
   form = new FormGroup({});
-  bills: Bill[] | undefined;
-  id = '0';
-  subscription: Subscription | null = null;
+  id = '';
+
+  private destroySubject = new Subject<void>();
 
   constructor(
     private billsFacade: BillsFacade,
     private router: Router,
-    private dataStorageService: DataStorageService,
     private billsService: BillsService
   ) {}
 
   ngOnInit(): void {
+    this.billsFacade.billsChanged$.pipe(
+      takeUntil(this.destroySubject),
+      tap(bills => {
+        this.id = bills.length.toString() + 1;
+      })
+    );
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    const { id, title, sum, startDate, endDate, payment } = this.form.value;
+    const newBill = { id, title, sum, startDate, endDate, payment };
+
+    this.billsService.postsBills(newBill).subscribe();
+    this.router.navigate(['/bills']);
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
+
+  private initForm(): void {
     this.form = new FormGroup({
       title: new FormControl('', Validators.required),
       sum: new FormControl('', [
@@ -37,33 +57,5 @@ export class NewBillComponent implements OnInit, OnDestroy {
       endDate: new FormControl('', Validators.required),
       payment: new FormControl('', Validators.required)
     });
-    this.subscription = this.billsFacade.billsChanged$.subscribe(
-      (bills: Bill[]) => {
-        this.bills = bills;
-        this.id = this.bills.length.toString();
-      }
-    );
-  }
-
-  onSubmit() {
-    let id = this.id + 1;
-    let title = this.form.value.title;
-    let sum = +this.form.value.sum;
-    let startDate = this.form.value.startDate;
-    let endDate = this.form.value.endDate;
-    let payment = this.form.value.payment;
-    const newBill: Bill = { id, title, sum, startDate, endDate, payment };
-    // this.billsFacade.addBill(newBill);
-    // this.billsFacade.billsChanged$.subscribe((bills: Bill[]) =>
-    //   this.bills = bills);
-    this.billsService
-      .postsBills(newBill)
-      .subscribe(response => console.log(response));
-    this.router.navigate(['/bills']);
-    this.billsFacade.addBill(newBill);
-  }
-
-  ngOnDestroy() {
-    this.subscription?.unsubscribe();
   }
 }
--- src/app/pages/bills/sotre/bills.service.ts
+++ src/app/pages/bills/sotre/bills.service.ts
@@ -36,6 +36,7 @@ export class BillsService {
   }
 
   postsBills(newBill: Bill) {
+    this.billsFacade.addBill(newBill);
     return from(addDoc(this.billsCollection, newBill));
   }
 }
